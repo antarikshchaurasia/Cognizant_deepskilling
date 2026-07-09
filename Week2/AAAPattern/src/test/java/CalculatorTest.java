bab1b02a06ebd64fbba042b9467d41d68ad8cdba
Antariksh Chaurasia <your-chaurasiaantariksh82@gmail.com>
2026-07-09 11:01:19 +0530

    commit

---
+++ Week2/AAAPattern/src/test/java/CalculatorTest.java
@@ -0,0 +1,49 @@
+import org.junit.After;
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CalculatorTest {
+
+    private Calculator calculator;
+
+    @Before
+    public void setUp() {
+        System.out.println("Setting up test data...");
+        calculator = new Calculator();
+    }
+
+    @After
+    public void tearDown() {
+        System.out.println("Cleaning up after test...");
+        calculator = null;
+    }
+
+    @Test
+    public void testAdd() {
+
+        // Arrange
+        int a = 10;
+        int b = 20;
+
+        // Act
+        int result = calculator.add(a, b);
+
+        // Assert
+        assertEquals(30, result);
+    }
+
+    @Test
+    public void testSubtract() {
+
+        // Arrange
+        int a = 20;
+        int b = 5;
+
+        // Act
+        int result = calculator.subtract(a, b);
+
+        // Assert
+        assertEquals(15, result);
+    }
+}
