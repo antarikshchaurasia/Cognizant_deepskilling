5d48d6c5464cd7bef67cbc6cfdb5f0390f4eb8f9
Antariksh Chaurasia <your-chaurasiaantariksh82@gmail.com>
2026-07-21 21:28:03 +0530

    Changes

---
+++ Week3/LibraryManagement/src/main/java/com/library/repository/BookRepository.java
@@ -0,0 +1,9 @@
+package com.library.repository;
+
+public class BookRepository {
+
+    public void showRepositoryStatus() {
+        System.out.println("BookRepository initialized successfully.");
+    }
+
+}
---
+++ Week3/MavenProjectConfiguration/src/main/java/com/library/MainApp.java
@@ -0,0 +1,11 @@
+package com.library;
+
+public class MainApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("Library Management Maven Project Configured Successfully.");
+
+    }
+
+}
